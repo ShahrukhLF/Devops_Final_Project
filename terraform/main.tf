4f294fa3460ef3798387e7fcb1fa0986021f1311
Shahrukh Karim <shahrukhkknmd@gmail.com>
2025-06-25 08:49:01 +0500

    Fixed SSH key path for Jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("${path.module}/public_key.pub")
 }
 
   os_disk {
